bf6b6520028f2a3219c2d9abddbb549595d90e36
eric <formateric@gmail.com>
2013-01-24 16:59:26 +0800

    tracking code

--- wp-content/themes/ericathy/footer.php
+++ wp-content/themes/ericathy/footer.php
@@ -1,5 +1,19 @@
 </section>
 <?php wp_footer(); ?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37930912-1']);
+  _gaq.push(['_setDomainName', 'ericathy.us']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <script src="<?php bloginfo('template_url'); ?>/js/jquery-1.8.3.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
 </body>
