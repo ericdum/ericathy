902f510ba1dc8d9ec0d2c58d984e889318c0fdb7
eric <formateric@gmail.com>
2013-05-03 17:16:47 +0800

    weixin

--- weixin.php
+++ weixin.php
@@ -1,3 +1,21 @@
 <?php
    $token = "JADHi21kdsalnlPKJWOlas183AH";
-echo $token;
+
+   $signature = @$_GET['signature'];
+   $timestamp = @$_GET['timestamp'];
+   $nonce = @$_GET['nonce'];
+   $echostr = @$_GET['echostr'];
+
+   $validation = array(
+       $token,
+       $timestamp,
+       $nonce,
+   );
+
+
+   sort($validation, SORT_STRING);
+   $validation = sha1(implode('', $validation));
+
+   if( $validation == $echostr ) {
+       echo $echostr;
+   }
